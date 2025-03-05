962f031be97f7e9c6cae71140cb28196720913e0
Emelie Boss <Emelie.boss@medieinstitutet.se>
2025-03-05 16:26:20 +0100

    feat(header): start header and nav structure

--- src/components/Header/MainHeader.vue
+++ src/components/Header/MainHeader.vue
@@ -4,11 +4,90 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <header>
+    <h1>Nemo´s Ice Cream</h1>
+    <img src="../../../assets/images/BurgerMenuWhite.svg" alt="Menu" class="whitehamburger">
+    <img src="../../../assets/images/BurgerMenuBrown.svg" alt="White clown fish" class="darkhamburger">
+
     <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <img src="../../../assets/images/nemo-solid.PNG" alt="" class="solidnemo">
+      <img src="../../../assets/images/silly-starfish.PNG" alt="A white starfish" class="sillystarfish">
+      <div class="navlinks">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Products</RouterLink>
+      </div>
     </nav>
   </header>
 </template>
 
-<style scoped lang="sass"></style>
+<style scoped lang="scss">
+
+header {
+  background-color: transparent;
+  height: 50px;
+}
+
+h1 {
+  color: #fff3e0;
+  font-family: 'Inter', sans-serif;
+  font-size: 24px;
+  padding-left: 24px;
+  position: absolute; // ändra sedan?
+  margin-top: 8px;
+  display: none; // Toggla vid knapptryck
+}
+
+nav {
+  display: none; // Toggla vid knapptryck
+}
+
+nav .solidnemo {
+  width: 74px;
+  height: 44px;
+  position: absolute;
+  transform: scaleX(-1);
+  top: 17vh;
+  left: 4vw;
+}
+
+nav .sillystarfish {
+  width: 73px;
+  height: 64px;
+  position: absolute;
+  top: 78vh;
+  right: 6vw;
+}
+
+.navlinks {
+  flex-direction: column;
+  position: absolute;
+  column-gap: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+
+}
+
+nav a {
+  color: #402F17;
+  font-family: 'Julius Sans One', sans-serif;
+  font-size: 70px;
+  text-decoration: none;
+  margin: 40px;
+}
+
+img {
+  width: 50px;
+  height: 50px;
+  float: right;
+  padding-right: 15px;
+}
+
+.whitehamburger {
+  display: none; // Toggla vid knapptryck
+}
+
+
+</style>

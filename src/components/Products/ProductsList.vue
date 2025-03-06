552f67e4b9535b7d7549cbd471810c8f6b07fc88
Jonatan Hellberg <Jonatan.Hellberg@medieinstitutet.se>
2025-03-06 14:53:11 +0100

    Add responsivity and final styling

--- src/components/Products/ProductsList.vue
+++ src/components/Products/ProductsList.vue
@@ -6,7 +6,7 @@ import products from '/src/components/Products/products.js';
     <article v-for="product in products" :key="product.id">
       <img :src="product.img.src" :alt="product.img.alt" :loading="product.img.loading" :width="product.img.width" :height="product.img.height">
       <div>
-        <h3>{{ product.name }}</h3>
+        <h3 class="heading-3">{{ product.name }}</h3>
         <p>{{ product.flavor }}</p>
       </div>
     </article>
@@ -16,22 +16,25 @@ import products from '/src/components/Products/products.js';
 article {
   display: flex;
   align-items: center;
-  margin-bottom: 52px;
+  margin-bottom: 46px;
   flex-direction: column;
+  overflow: hidden;
+  padding-bottom: 6px;
+  max-width: 350px;
+
 
   img, div {
     box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
   }
 
   img {
-  border-radius: 50%;
-  margin-right: 1rem;
-  width: 300px;
-  height: 300px;
-  object-fit: cover;
+    border-radius: 50%;
+    width: 300px;
+    height: 300px;
+    object-fit: cover;
   }
 
-  div{
+  div {
     background-color: var(--bg-button);
     color: var(--text-light);
     margin-top: 1rem;

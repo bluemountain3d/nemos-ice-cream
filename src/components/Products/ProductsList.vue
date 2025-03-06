f7c7b806939dbce41f8341c6375fcb7b9fdcb5bb
Jonatan Hellberg <Jonatan.Hellberg@medieinstitutet.se>
2025-03-06 10:17:11 +0100

    Add styling to the product elements

--- src/components/Products/ProductsList.vue
+++ src/components/Products/ProductsList.vue
@@ -12,4 +12,50 @@ import products from '/src/components/Products/products.js';
     </article>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+article {
+  display: flex;
+  align-items: center;
+  margin-bottom: 52px;
+  flex-direction: column;
+
+  img, div {
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  }
+
+  img {
+  border-radius: 50%;
+  margin-right: 1rem;
+  width: 300px;
+  height: 300px;
+  object-fit: cover;
+  }
+
+  div{
+    background-color: var(--bg-button);
+    color: var(--text-light);
+    margin-top: 1rem;
+    min-width: 235px;
+    height: 65px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+
+    h3, p {
+      margin: 0;
+    }
+
+
+    h3 {
+      font-size: var(--fs-200);
+      line-height: 150%;
+      font-weight: 500;
+    }
+
+    p{
+      font-size: var(--fs-100);
+    }
+  }
+}
+</style>

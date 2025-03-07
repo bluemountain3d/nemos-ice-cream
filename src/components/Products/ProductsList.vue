13a528c44936784a85ae099f79f01e9755367c90
Jonatan Hellberg <Jonatan.Hellberg@medieinstitutet.se>
2025-03-07 10:55:31 +0100

    Add dynamic measurments instead of static and formatting

--- src/components/Products/ProductsList.vue
+++ src/components/Products/ProductsList.vue
@@ -3,27 +3,33 @@ import products from '/src/components/Products/products.js';
 </script>
 
 <template>
-    <article v-for="product in products" :key="product.id">
-      <img :src="product.img.src" :alt="product.img.alt" :loading="product.img.loading" :width="product.img.width" :height="product.img.height">
-      <div>
-        <h3 class="heading-3">{{ product.name }}</h3>
-        <p>{{ product.flavor }}</p>
-      </div>
-    </article>
+  <article v-for="product in products" :key="product.id">
+    <img
+      :src="product.img.src"
+      :alt="product.img.alt"
+      :loading="product.img.loading"
+      :width="product.img.width"
+      :height="product.img.height"
+    />
+    <div>
+      <h3 class="heading-3">{{ product.name }}</h3>
+      <p>{{ product.flavor }}</p>
+    </div>
+  </article>
 </template>
 
 <style scoped lang="scss">
 article {
   display: flex;
   align-items: center;
-  margin-bottom: 46px;
+  margin-bottom: 2.875rem;
   flex-direction: column;
   overflow: hidden;
-  padding-bottom: 6px;
-  max-width: 350px;
+  padding-bottom: 0.375rem;
+  max-width: 21.875rem;
 
-
-  img, div {
+  img,
+  div {
     box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
   }
 
@@ -38,17 +44,17 @@ article {
     background-color: var(--bg-button);
     color: var(--text-light);
     margin-top: 1rem;
-    min-width: 235px;
-    height: 65px;
+    min-width: 14.688rem;
+    height: 4.063rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
     text-align: center;
 
-    h3, p {
+    h3,
+    p {
       margin: 0;
     }
-
 
     h3 {
       font-size: var(--fs-200);
@@ -56,7 +62,7 @@ article {
       font-weight: 500;
     }
 
-    p{
+    p {
       font-size: var(--fs-100);
     }
   }

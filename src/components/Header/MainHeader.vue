6baa4c4f5522bef86606a27c6c5c7ba2e8d637e3
Egil Eskilsson <egil@egileskilsson.se>
2025-03-11 12:10:16 +0100

    refactor: Refactored header that did'nt work properly, added *.bak to gitignore

--- src/components/Header/MainHeader.vue
+++ src/components/Header/MainHeader.vue
@@ -1,136 +1,200 @@
-<script setup>
-import { RouterLink } from 'vue-router';
-import { ref } from 'vue';
-
-const showElement = ref(false);
-
-function hideElement() {
-  showElement.value = !showElement.value;
-}
-</script>
-
 <template>
-  <header>
-      <p>Nemo's Ice Cream</p>
-    <button
-      @click="hideElement"
-      id="menuButton"
-      :style="{
-        backgroundImage: showElement
-          ? 'url(./assets/images/x.svg)'
-          : 'url(./assets/images/BurgerMenuBrown.svg)',
-      }"
-    ></button>
-      <nav v-if="showElement">
-        <img src="../../assets/images/nemo-solid.webp" alt="A white clown-fish" id="solidnemo" />
-        <img
-          src="../../assets/images/silly-starfish.webp"
-          alt="A white starfish"
-          id="sillystarfish"
-        />
-        <div class="navlinks">
-          <RouterLink to="/" @click="hideElement">Home</RouterLink>
-          <RouterLink to="/about" @click="hideElement">About</RouterLink>
-          <RouterLink to="/#products" @click="hideElement">Products</RouterLink>
-        </div>
+  <header class="header">
+    <div class="header__inner">
+      <p class="header__brand">Nemos' Ice Cream</p>
+      <button class="header__menu-toggle" @click="toggleMenu" aria-label="Toggle menu">
+        <img :src="isMenuOpen ? closeMenuIcon : openMenuIcon" alt="Menu icon" />
+      </button>
+      <nav class="header__nav" :class="{ 'header__nav--open': isMenuOpen }">
+        <ul class="header__menu">
+          <li class="header__menu-item">
+            <RouterLink to="/" class="header__menu-link" @click="toggleMenu"> Home </RouterLink>
+          </li>
+          <li class="header__menu-item">
+            <RouterLink to="/about" class="header__menu-link" @click="toggleMenu">
+              About
+            </RouterLink>
+          </li>
+          <li class="header__menu-item">
+            <RouterLink to="/#products" class="header__menu-link" @click="toggleMenu">
+              Products
+            </RouterLink>
+          </li>
+          <img
+            class="header__fish-image"
+            :src="nemoSolid"
+            alt="handdrawn outline image of an clownfish"
+          />
+          <img
+            class="header__starfish-image"
+            :src="sillyStarfish"
+            alt="handdrawn outline image of an starfish"
+          />
+        </ul>
       </nav>
+    </div>
   </header>
 </template>
 
+<script setup>
+// Javascript here
+import { ref } from 'vue';
+import openMenuIcon from '@/assets/images/BurgerMenuWhite.svg';
+import closeMenuIcon from '@/assets/images/x.svg';
+import nemoSolid from '@/assets/images/nemo-solid.webp';
+import sillyStarfish from '@/assets/images/silly-starfish.webp';
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+</script>
+
 <style scoped lang="scss">
-// TODO:
+// Local Component Specific styling
 
-// ----> pixelvärdet / 16 använd REM
-// Skapa media queries.
-// Om tid finns: kolla på headerbakgrunds-fix.
-header {
-  background-color: transparent;
-  position: sticky;
+.header {
+  z-index: 999;
+  width: 100%;
+  display: block;
+  position: fixed;
   top: 0;
-  z-index: 10;
-  max-width: 100vw;
-}
-
-p {
-  color: var(--text-light);
-  font-family: var(--ff-secondary);
-  font-size: 24px;
-  padding-left: 24px;
-  position: fixed;
-  top: -24px;
-  height: 50px;
-  width: 100%;
-  font-weight: 800;
-  padding-top: 7px;
+  left: 0;
   background-color: var(--bg-header);
-}
 
-button {
-  z-index: 2;
-  width: 50px;
-  height: 50px;
-  right: 15px;
-  background-color: transparent;
-  background-position: center;
-  background-size: cover;
-  border: 0;
-  transition:
-    background-image 0.3s ease-in-out,
-    opacity 0.3s ease-in-out;
-  position: fixed;
-}
+  &__inner {
+    width: min(100%, 1440px);
+    margin-inline: auto;
+    padding-inline: 5.7vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-nav #solidnemo {
-  width: 67px;
-  height: 44px;
-  position: absolute;
-  transform: scaleX(-1);
-  top: 17vh;
-  left: 4vw;
-}
+  &__brand {
+    font-size: var(--fs-300);
+    line-height: 1;
+    margin: 0;
+    color: var(--text-light);
 
-nav #sillystarfish {
-  width: 63px;
-  height: 64px;
-  position: absolute;
-  top: 78vh;
-  right: 6vw;
-}
+    @media screen and (min-width: 720px) {
+      font-size: var(--fs-400);
+    }
+  }
 
-.navlinks {
-  background-color: var(--bg-main);
-  z-index: 9;
-  flex-direction: column;
-  position: absolute;
-  column-gap: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100%;
-  z-index: -1;
-  overflow-x: hidden;
-}
+  &__menu-toggle {
+    background: none;
+    border: none;
+    cursor: pointer;
+    width: 4.6875rem;
+    height: 4.6875rem;
+    display: grid;
+    place-content: center;
 
-nav a {
-  color: var(--text-primary);
-  font-family: var(--ff-primary);
-  font-size: 70px;
-  text-decoration: none;
-  margin: 40px;
-}
+    & * {
+      pointer-events: none;
+    }
 
-// Fade in / out elements
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-enter-to,
-.fade-leave-from {
-  opacity: 1;
+    @media screen and (min-width: 1024px) {
+      width: 3.75rem;
+      height: 3.75rem;
+    }
+  }
+
+  &__nav {
+    // nav
+    display: none;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    height: calc(100vh - 100%);
+    background-color: var(--bg-main);
+
+    &--open {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+
+  &__fish-image,
+  &__starfish-image {
+    display: block;
+    position: absolute;
+    height: auto;
+  }
+
+  &__fish-image {
+    top: 0;
+    left: 0;
+    width: 16.41vw;
+    @media screen and (min-width: 720px) {
+      width: 22.78vw;
+    }
+    @media screen and (min-width: 1024px) {
+      width: 18.82vw;
+    }
+  }
+
+  &__starfish-image {
+    bottom: 0;
+    right: 0;
+    width: 15.38vw;
+    @media screen and (min-width: 720px) {
+      width: 20.74vw;
+    }
+    @media screen and (min-width: 1024px) {
+      width: 16.04vw;
+    }
+  }
+
+  &__menu {
+    // ul
+    position: relative;
+    list-style: none;
+    padding: 4vh 0;
+    width: 100%;
+    margin: 0 7%;
+    display: grid;
+    justify-content: center;
+  }
+
+  &__menu-item {
+    // li
+    width: 100%;
+    max-width: 29.25rem;
+
+    &:not(:last-of-type) {
+      margin-bottom: 7.5vh;
+
+      @media screen and (min-width: 1024px) {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  &__menu-link {
+    // RouterLink
+    display: block;
+    width: 100%;
+    text-align: center;
+    font-size: 3.5rem;
+    font-family: var(--ff-primary);
+    text-decoration: none;
+    color: var(--text-primary);
+    filter: drop-shadow(0 0.25rem 0.125rem rgba(0, 0, 0, 0.25));
+    padding-block: 0.5em;
+
+    &:hover {
+      cursor: pointer;
+      opacity: 0.5;
+    }
+
+    @media screen and (min-width: 720px) {
+      font-size: 4.375rem;
+    }
+  }
 }
 </style>
